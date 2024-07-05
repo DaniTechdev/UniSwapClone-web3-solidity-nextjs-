8aefcdf29ffcb2991854d14f46a7bcdc9bdd7023
DaniTech <tochidan11@gmail.com>
2024-07-05 05:56:37 -0700

    Done with the basic skeleton of the Navbar and styling

--- Components/NavBar/NavBar.jsx
+++ Components/NavBar/NavBar.jsx
@@ -1,7 +1,85 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+//INTERNAL IMPORT
+import Style from "./NavBar.module.css";
+import images from "../../assets";
+import { Model, TokenList } from "../index";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  const menuItems = [
+    {
+      name: "Swap",
+      link: "/",
+    },
+    {
+      name: "Tokens",
+      link: "/",
+    },
+    {
+      name: "Pools",
+      link: "/",
+    },
+  ];
+
+  const [openModel, setOpenModel] = useState(false);
+  const [openTokenBox, setOpenTokenBox] = useState(false);
+
+  return (
+    <div className={Style.Navbar}>
+      <div className={Style.Navbar_box}>
+        <div className={Style.Navbar_box_left}>
+          {/* {//LOGO IMAGE} */}
+          <div className={Style.Navbar_box_left_img}>
+            <Image src={images.uniswap} width={50} height={50} alt="logo" />
+          </div>
+
+          {/* MENU ITEMS */}
+
+          <div className={Style.Navbar_box_left_menu}>
+            {menuItems.map((el, i) => (
+              <Link
+                key={i + 1}
+                href={{ pathname: `${el.name}`, query: `${el.link}` }}
+              >
+                <p className={Style.Navbar_box_left_menu_item}>{el.name}</p>
+              </Link>
+            ))}
+          </div>
+        </div>
+        {/* MIDDLE SECTION */}
+        <div className={Style.Navbar_box_middle}>
+          <div className={Style.Navbar_box_middle_search}>
+            <div className={Style.Navbar_box_middle_search_img}>
+              <Image src={images.search} alt="search" width={20} height={20} />
+            </div>
+            {/* INPUT SECTION */}
+            <input type="text" placeholder="Search Tokens" />
+          </div>
+        </div>
+        {/* RIGHT SECTION */}
+        <div className={Style.Navbar_box_right}>
+          <div className={Style.Navbar_box_right_box}>
+            <div className={Style.Navbar_box_right_box_img}>
+              <Image src={images.ether} alt="Network" height={30} width={30} />
+            </div>
+            <p> Network Name</p>
+          </div>
+          <button onClick={() => {}}>Address</button>
+
+          {openModel && (
+            // sending setOpenModel into Model to use it and close the Modal
+            <Model setOpenModel={setOpenModel} connectWallet="Connect" />
+          )}
+        </div>
+      </div>
+      {/* TOKENLIST COMPONENT */}
+      {openTokenBox && (
+        <TokenList tokenData="Hey" setOpenTokenBox={setOpenTokenBox} />
+      )}
+    </div>
+  );
 };
 
 export default NavBar;
--- Components/index.js
+++ Components/index.js
@@ -1,5 +1,5 @@
 import HeroSection from "./HeroSection/HeroSection";
-import Model from "./Model";
+import Model from "./Model/Model";
 import NavBar from "./NavBar/NavBar";
 import SearchToken from "./SearchToken/SearchToken";
 import Toggle from "./Toggle/Toggle";
