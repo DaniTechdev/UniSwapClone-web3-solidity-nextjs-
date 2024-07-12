63ccf9d8b2fccd8fac28fe24213c229840739a27
DaniTech <tochidan11@gmail.com>
2024-07-11 22:47:29 -0700

    Done with connecting to BooToken function, getting the contract object of the BooToken

--- Utils/appFeatures.js
+++ Utils/appFeatures.js
@@ -15,7 +15,6 @@ import {
 } from "../Context/constants";
 
 //CHECK IF WALLET IS CONNECTED
-
 export const checkIfWalletConnected = async () => {
   try {
     if (!window.ethereum) return console.log("Install MetaMast");
@@ -31,7 +30,6 @@ export const checkIfWalletConnected = async () => {
 };
 
 //Connect Wallet
-
 export const connectWallet = async () => {
   try {
     if (!window.ethereum) return console.log("Install MetaMast");
@@ -41,6 +39,24 @@ export const connectWallet = async () => {
 
     const firstAccount = accounts[0];
     return firstAccount;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//FETCHING CONTRACT====================
+
+//BOO TOKEN FETCHING
+export const fetchBooContract = (signerOrProvider) =>
+  new ethers.Contract(BooTokenAddress, BooTokenABI, signerOrProvider);
+//CONNECTING WITH BOO TOKEN CONTRACT
+export const connectingWithBooToken = async () => {
+  try {
+    const web3modal = new Web3Modal();
+    const connection = await web3modal.connect();
+    const provider = new ethers.providers.Web3Provider(connection);
+    const signer = provider.getSigner();
+    const contract = fetchBooContract(signer);
   } catch (error) {
     console.log(error);
   }
