6b2dee332fef691cbbc2a7d1bfb691e56bc55c56
DaniTech <tochidan11@gmail.com>
2024-07-12 00:01:28 -0700

    Renamed appFeature to apiFeatures

---
+++ Context/SwapContext.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+import { ethers, BigNumber } from "ethers";
+import Web3Modal from "web3modal";
+
+//INTERNAL IMPORT
+import {} from "../Utils/apiFeatures";
+const SwapContext = () => {
+  return <div>SwapContext</div>;
+};
+
+export default SwapContext;
