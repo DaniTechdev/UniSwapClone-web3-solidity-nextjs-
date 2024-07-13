22f774ae8c6631c6a21740da24439616fb1a1b72
DaniTech <tochidan11@gmail.com>
2024-07-13 09:49:16 -0700

    Done with the functions of collecting transaction fee from every swap

--- contracts/Liquidity.sol
+++ contracts/Liquidity.sol
@@ -150,5 +150,52 @@ contract LiquidityExamples is IERC721Receiver {
         TransferHelper.safeTransfer(USDC,msg.sender,refund1);
     }
     }
- 
+
+
+    //functions to increate or decrease liquidity fee, functions to remove or decrease the liquidity added into the liquidity pool
+
+
+    //function to collectAllFee when someoen do the transactions/swap
+    function collectAllFees () external returns (uint256 amount0, uint256 amount1){
+        //set amount0Max and amount1Max to uint256.max to collect all fees
+        //alternately can set recipient to msg.sender and avoid another transaction in "sendToOwner"
+
+        INonfungiblePositionManager.CollectParams memory params = INonfungiblePositionManager.CollectParams({
+            tokenId:tokenId,
+            recipient:address(this),
+            amount0Max: type(uint128).max,
+            amount1Max: type(uint128).max,
+        })
+        (amount0,amount1) = nonfungiblePositionManager.collect(params)
+
+        console.log("fee o", amount0);
+        console.log("fee 1", amount1);
+    }
+
+
+        //function to increase current liquidity
+   function increaseLiquidityCurrentRange ( uint256 amountAdd0, uint256 amountAdd1) external returns(uint128 liquidity, uint256 amount0, uint256 amount0, uint256 amount1) {
+    TransferHelper.safeTransferFrom(DAI,msg.sender, address(this), amountAdd0);
+    TransferHelper.safeTransferFrom(USDC, msg.sender, address(this), amountAdd1);
+
+    TransferHelper.safeApprove(DAI, address(nonfungiblePositionManager), amountAdd0);
+    TransferHelper.safeApprove(USDC,address(nonfungiblePositionManager),amountAdd1);
+
+
+    INonfungiblePositionManager.IncreaseLiquidityParams memory params = INonfungiblePositionManager.IncreaseLiquidityParams({
+        tokenId:tokenId,
+        amount0Desired:amountAdd0,
+        amount1Desired:amountAdd1,
+        amount0Min:0,
+        amount1Min:0,
+        deadline:block.timestamp,
+    });
+
+    (liquidity,amount0,amount1) = nonfungiblePositionManager.increaseLiquidity(params);
+
+    console.log("Liquidity", liquidity);
+    console.log("amount0", amount0);
+    console.log("amount1", amount1);
+
+   } 
 }
