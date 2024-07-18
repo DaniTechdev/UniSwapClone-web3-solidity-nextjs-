b14ac7958e3d90e8c001183549ec1c61166e6458
DaniTech <tochidan11@gmail.com>
2024-07-18 04:08:47 -0700

    Created skeletal form of the PoolAdd and PoolConnect components

---
+++ Components/PoolAdd/PoolAdd.jsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react";
+
+//INTERNAL IMPORT
+import images from "../../assets";
+
+const PoolAdd = () => {
+
+    const [openModel, setOpenModel] = useState(false)
+    const [openTokenModel, setOpenTokenModel] = useState(false)
+    const [active, setActive] = useState(1)
+    const [openFee, setOpenFee] = useState(false)
+    const [minPrice, setMinPrice] = useState(0)
+    const [maxPrice, setMaxPrice] = useState(0)
+
+    const feePairs = [
+        {
+            fee:0.5%,
+            info:"Best for stable pairs",
+            number: "0% select"
+        },
+        {
+            fee:"0.3%",
+            info:"Best for stable pairs",
+            number:"0% select"
+        },
+        {
+            fee:"1%",
+            info:"Best for stable pairs",
+            number: "0% select"
+        }
+
+    ]
+
+     const mintPriceRange = (text)=>{
+        if(text == "+"){
+            setMinPrice(minPrice + 1)
+        }else if(text == "-"){
+            setMaxPrice(maxPrice -1)
+        }
+     }
+
+  return (
+    <div className={Style.PoolAdd}>
+    <div className={Style.PoolAdd_box}>
+    <div className={Style.PoolAdd_box}></div>
+    </div>
+    </div>
+  )
+};
+
+export default PoolAdd;
--- pages/Pools.js
+++ pages/Pools.js
@@ -1,7 +1,19 @@
 import React from "react";
+import Image from "next/image";
+
+//INTERNAL IMPORT
+import images from "../assets";
+import Style from "../styles/Tokens.module.css";
+
+import { PoolConnect, PoolAdd } from "../Components/index";
 
 const Pools = () => {
-  return <div>Pools</div>;
+  return (
+    <div className={Style.Pool}>
+      <PoolAdd />
+      {/* <PoolConnect /> */}
+    </div>
+  );
 };
 
 export default Pools;
