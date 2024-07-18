19b221437723464d4e2c43ae1bd5035f20368676
DaniTech <tochidan11@gmail.com>
2024-07-18 04:25:59 -0700

    Created mintPriceRange function

--- Components/PoolAdd/PoolAdd.jsx
+++ Components/PoolAdd/PoolAdd.jsx
@@ -1,51 +1,56 @@
 import React, { useState, useEffect } from "react";
+import Image from "next/image";
 
 //INTERNAL IMPORT
 import images from "../../assets";
+import Style from "./PoolAdd.module.css";
 
 const PoolAdd = () => {
+  const [openModel, setOpenModel] = useState(false);
+  const [openTokenModel, setOpenTokenModel] = useState(false);
+  const [active, setActive] = useState(1);
+  const [openFee, setOpenFee] = useState(false);
+  const [minPrice, setMinPrice] = useState(0);
+  const [maxPrice, setMaxPrice] = useState(0);
 
-    const [openModel, setOpenModel] = useState(false)
-    const [openTokenModel, setOpenTokenModel] = useState(false)
-    const [active, setActive] = useState(1)
-    const [openFee, setOpenFee] = useState(false)
-    const [minPrice, setMinPrice] = useState(0)
-    const [maxPrice, setMaxPrice] = useState(0)
+  const feePairs = [
+    {
+      fee: "0.5 %",
+      info: "Best for stable pairs",
+      number: "0% select",
+    },
+    {
+      fee: "0.3%",
+      info: "Best for stable pairs",
+      number: "0% select",
+    },
+    {
+      fee: "1%",
+      info: "Best for stable pairs",
+      number: "0% select",
+    },
+  ];
 
-    const feePairs = [
-        {
-            fee:0.5%,
-            info:"Best for stable pairs",
-            number: "0% select"
-        },
-        {
-            fee:"0.3%",
-            info:"Best for stable pairs",
-            number:"0% select"
-        },
-        {
-            fee:"1%",
-            info:"Best for stable pairs",
-            number: "0% select"
-        }
-
-    ]
-
-     const mintPriceRange = (text)=>{
-        if(text == "+"){
-            setMinPrice(minPrice + 1)
-        }else if(text == "-"){
-            setMaxPrice(maxPrice -1)
-        }
-     }
+  const mintPriceRange = (text) => {
+    if (text == "+") {
+      setMinPrice(minPrice + 1);
+    } else if (text == "-") {
+      setMaxPrice(maxPrice - 1);
+    }
+  };
 
   return (
     <div className={Style.PoolAdd}>
-    <div className={Style.PoolAdd_box}>
-    <div className={Style.PoolAdd_box}></div>
+      <div className={Style.PoolAdd_box}>
+        <div className={Style.PoolAdd_box_header}>
+          <div className={Style.PoolAdd_box_header}>
+            <Image src={images.arrowLeft} alt="image" width={30} height={30} />
+          </div>
+          <div className={Style.PoolAdd_box_header_middle}></div>
+        </div>
+      </div>
     </div>
-    </div>
-  )
+  );
 };
 
 export default PoolAdd;
