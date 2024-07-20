c46934351d929981e9aaed0d81b785a4917493ca
DaniTech <tochidan11@gmail.com>
2024-07-20 06:32:15 -0700

    installed @uniswap/smart-order-route to get updated price

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,21 +2,21 @@ const hre = require("hardhat");
 
 async function main() {
   //ERC20 BOO TOKEN
-  const BooToken = await hre.ethers.getContractFactory("BooToken");
-  const booToken = await BooToken.deploy();
-  await booToken.deployed();
-  console.log(
-    ` BooToken deployed to the contract address: ${booToken.address}`
-  );
+  // const BooToken = await hre.ethers.getContractFactory("BooToken");
+  // const booToken = await BooToken.deploy();
+  // await booToken.deployed();
+  // console.log(
+  //   ` BooToken deployed to the contract address: ${booToken.address}`
+  // );
   // console.log(booToken);
 
   //ERC20 LifeToken TOKEN
-  const LifeToken = await hre.ethers.getContractFactory("LifeToken");
-  const lifeToken = await LifeToken.deploy();
-  await lifeToken.deployed();
-  console.log(
-    ` LifeToken  deployed to the contract address: ${lifeToken.address}`
-  );
+  // const LifeToken = await hre.ethers.getContractFactory("LifeToken");
+  // const lifeToken = await LifeToken.deploy();
+  // await lifeToken.deployed();
+  // console.log(
+  //   ` LifeToken  deployed to the contract address: ${lifeToken.address}`
+  // );
   // console.log(lifeToken);
 
   // SingleSwapToken contract
@@ -31,12 +31,12 @@ async function main() {
   // console.log( SingleSwapToken);
 
   //  SwapMultiHop
-  const SwapMultiHop = await hre.ethers.getContractFactory("SwapMultiHop");
-  const swapMultiHop = await SwapMultiHop.deploy();
-  await swapMultiHop.deployed();
-  console.log(
-    `  SwapMultiHop deployed to the contract address: ${swapMultiHop.address}`
-  );
+  // const SwapMultiHop = await hre.ethers.getContractFactory("SwapMultiHop");
+  // const swapMultiHop = await SwapMultiHop.deploy();
+  // await swapMultiHop.deployed();
+  // console.log(
+  //   `  SwapMultiHop deployed to the contract address: ${swapMultiHop.address}`
+  // );
   // console.log(  SwapMultiHop);
 }
 
